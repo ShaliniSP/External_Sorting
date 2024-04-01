b56617bcaeaf258d79513628af43a31ce5aca356
saiprasa-usc <131034932+saiprasa-usc@users.noreply.github.com>
2024-04-01 11:35:02 -0700

    ssb join done

--- ssb_join.py
+++ ssb_join.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def simple_sort_based_join(product_filename, maker_filename, output_filename):
     """
     Executes a simple sort-based join between two datasets and writes the output to a CSV file.
@@ -9,6 +8,27 @@ def simple_sort_based_join(product_filename, maker_filename, output_filename):
     :param maker_filename: Filename of the sorted maker dataset.
     :param output_filename: Filename for the output joined dataset.
     """
+
+    product_file = open(product_filename, 'r')
+    maker_file = open(maker_filename, 'r')
+    output_file = open(output_filename, 'w', newline='')
+
+    product_reader = csv.reader(product_file)
+    maker_reader = csv.reader(maker_file)
+    joined_writer = csv.writer(output_file)
+
+    product_row = next(product_reader, None)
+    maker_row = next(maker_reader, None)
+
+    while product_row is not None and maker_row is not None:
+        if product_row[0] == maker_row[0]:
+            joined_writer.writerow([product_row[0], product_row[1], maker_row[1]])
+            product_row = next(product_reader, None)
+            maker_row = next(maker_reader, None)
+        elif product_row[0] < maker_row[0]:
+            product_row = next(product_reader, None)
+        else:
+            maker_row = next(maker_reader, None)
 
 
 if __name__ == "__main__":
